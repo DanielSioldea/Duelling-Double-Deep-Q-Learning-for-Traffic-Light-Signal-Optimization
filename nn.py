a7e18c0c3856c16529abfa718b035a3f881f4158
BoofinBool <sioldead@mcmaster.ca>
2024-04-17 15:36:21 -0400

    Adjusted agent and model classes

--- nn.py
+++ nn.py
@@ -9,6 +9,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
 # SET UP SUMO TRACI CONNECTION
@@ -31,93 +32,6 @@ def get_options():
                          default=False, help="run the commandline version of sumo")
     options, args = optParser.parse_args()
     return options
-
-## NEURAL NETWORK
-class TrafficController(nn.Module):
-    def __init__(self, input_size, hidden1_size, hidden2_size, output_size):
-        super(TrafficController, self).__init__()
-        self.input_size = input_size
-        self.hidden1_size = hidden1_size
-        self.hidden2_size = hidden2_size
-        self.output_size = output_size
-
-        #self.flatten = nn.Flatten()
-        self.hidden1 = nn.Linear(input_size, hidden1_size)      # FIRST HIDDEN LAYER HAS 12 NEURONS
-        self.activ1 = nn.ReLU()                                 # ReLU ACTIVATION FUNCTION
-        self.hidden2 = nn.Linear(hidden1_size, hidden2_size)    # SECOND HIDDEN LAYER HAS 8 NEURONS
-        self.activ2 = nn.ReLU()                                 # ReLU ACTIVATION FUNCTION
-        self.output = nn.Linear(hidden2_size, output_size)      # OUTPUT LAYER HAS ONE NEURON
-        self.activ_out = nn.Sigmoid()                           # SIGMOID ACTIVATION FUNCTION (ENSURES OUTPUT BETWEEN 0 AND 1)
-
-        # OPTIMIZER AND LOSS FUNCTION (TEST DIFFERENT OPTIONS)
-        self.optim = optim.Adam(self.parameters(), lr=0.001)
-        self.loss = nn.MSELoss()
-        # SELECT GPU OR CPU DEPENDING ON WHAT IS AVAILABLE
-        self.device = ("cuda" if torch.cuda.is_available() else "cpu")
-        self.to(self.device)
-
-        print(f"Network is using {self.device} device.")
-    
-    # DEFINE HOW NN FEEDS FORWARD INTO LAYERS
-    def forward(self, x):
-        x = self.activ1(self.hidden1(x))
-        x = self.activ2(self.hidden2(x))
-        x = self.activ_out(self.output(x))
-        return x
-    
-## DEFINE AGENT CLASS FOR LEARNING
-class TrafficAgent:
-    def __init__(self, gamma, epsilon, lr, input_size, hidden1_size, hidden2_size, n_actions, max_memory_size, batch_size):
-        self.gamma = gamma
-        self.epsilon = epsilon
-        self.lr = lr
-        self.input_size = input_size
-        self.hidden1_size = hidden1_size
-        self.hidden2_size = hidden2_size
-        self.n_actions = n_actions
-        max_memory_size = 100000
-        self.mem_size = max_memory_size
-        self.mem_cntr = 0
-        self.batch_size = batch_size
-
-        self.q_eval = TrafficController(self.input_size, self.hidden1_size, self.hidden2_size, self.n_actions)
-
-    def choose_action(self, observation):
-        if np.random.random() > self.epsilon:
-            state = torch.tensor([observation], dtype=torch.float32)
-            actions = self.q_eval.forward(state)
-            action = torch.argmax(actions).item()
-        else:
-            action = np.random.choice(self.n_actions)
-        return action
-    
-    def learn(self):
-        self.q_eval.optim.zero_grad()
-        if self.mem_cntr < self.batch_size:
-            return
-        max_mem = min(self.mem_cntr, self.mem_size)
-        batch = np.random.choice(max_mem, self.batch_size, replace=False)
-
-        state_batch = self.state_memory[batch]
-        action_batch = self.action_memory[batch]
-        reward_batch = self.reward_memory[batch]
-        new_state_batch = self.new_state_memory[batch]
-        done_batch = self.terminal_memory[batch]
-
-        state_batch = torch.tensor(state_batch).to(self.q_eval.device)
-        action_batch = torch.tensor(action_batch).to(self.q_eval.device)
-        reward_batch = torch.tensor(reward_batch).to(self.q_eval.device)
-        new_state_batch = torch.tensor(new_state_batch).to(self.q_eval.device)
-        done_batch = torch.tensor(done_batch).to(self.q_eval.device)
-
-        q_eval = self.q_eval.forward(state_batch).gather(1, action_batch.unsqueeze(1)).squeeze(1)
-        q_next = self.q_eval.forward(new_state_batch).max(dim=1)[0]
-        q_next[done_batch] = 0.0
-        q_target = reward_batch + self.gamma * q_next
-
-        loss = self.q_eval.loss(q_target, q_eval).to(self.q_eval.device)
-        loss.backward()
-        self.q_eval.optim.step()
 
 # OBTAIN QUEUE LENGTHS AND TIMES
 def queue_info(lane):
@@ -192,7 +106,123 @@ def adjust_traffic_light(junctions, junc_time, junc_state):
     for i, junction in enumerate(junctions):
         traci.trafficlight.setRedYellowGreenState(junction, junc_state[i])
         traci.trafficlight.setPhaseDuration(junction, junc_time[i])
-        print(f"Junction {junction} state: {junc_state[i]}, time: {junc_time[i]} at junction {i}")
+        # print(f"Junction {junction} state: {junc_state[i]}, time: {junc_time[i]} at junction {i}")
+
+## NEURAL NETWORK
+class TrafficController(nn.Module):
+    def __init__(self, input_size, lr, hidden1_size, hidden2_size, output_size):
+        super(TrafficController, self).__init__()
+        self.input_size = input_size
+        self.hidden1_size = hidden1_size
+        self.hidden2_size = hidden2_size
+        self.output_size = output_size
+
+        # print(f"input_size: {self.input_size}, type: {type(self.input_size)}")
+        # print(f"hidden1_size: {self.hidden1_size}, type: {type(self.hidden1_size)}")
+        # print(f"hidden2_size: {self.hidden2_size}, type: {type(self.hidden2_size)}")
+        # print(f"output_size: {self.output_size}, type: {type(self.output_size)}")
+
+        #self.flatten = nn.Flatten()
+        self.hidden1 = nn.Linear(self.input_size, self.hidden1_size)      # FIRST HIDDEN LAYER HAS 12 NEURONS
+        # self.activ1 = nn.ReLU()                                 # ReLU ACTIVATION FUNCTION
+        self.hidden2 = nn.Linear(self.hidden1_size, self.hidden2_size)    # SECOND HIDDEN LAYER HAS 8 NEURONS
+        # self.activ2 = nn.ReLU()                                 # ReLU ACTIVATION FUNCTION
+        self.output = nn.Linear(self.hidden2_size, self.output_size)      # OUTPUT LAYER HAS ONE NEURON
+        # self.activ_out = nn.Sigmoid()                           # SIGMOID ACTIVATION FUNCTION (ENSURES OUTPUT BETWEEN 0 AND 1)
+
+        # OPTIMIZER AND LOSS FUNCTION (TEST DIFFERENT OPTIONS)
+        self.optim = optim.Adam(self.parameters(), lr=lr)
+        self.loss = nn.MSELoss()
+        # SELECT GPU OR CPU DEPENDING ON WHAT IS AVAILABLE
+        self.device = ("cuda" if torch.cuda.is_available() else "cpu")
+        self.to(self.device)
+
+        print(f"Network is using {self.device} device.")
+    
+    # DEFINE HOW NN FEEDS FORWARD INTO LAYERS
+    def forward(self, x):
+        # x = self.activ1(self.hidden1(x))
+        # x = self.activ2(self.hidden2(x))
+        # x = self.activ_out(self.output(x))
+        x = F.relu(self.hidden1(x))
+        x = F.relu(self.hidden2(x))
+        actions = self.output(x)
+        return actions
+    
+## DEFINE AGENT CLASS FOR LEARNING
+class TrafficAgent:
+    def __init__(self, gamma, epsilon, lr, input_size, hidden1_size, hidden2_size, n_actions, max_memory_size, batch_size, eps_end=0.01, eps_dec=5e-4):
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.eps_min = eps_end
+        self.eps_dec = eps_dec
+        self.lr = lr
+        self.input_size = input_size
+        self.hidden1_size = hidden1_size
+        self.hidden2_size = hidden2_size
+        self.action_space = [i for i in range(n_actions)]
+        max_memory_size = 100000
+        self.mem_size = max_memory_size
+        self.mem_cntr = 0
+        self.batch_size = batch_size
+
+        self.q_eval = TrafficController(self.lr, self.input_size, self.hidden1_size, self.hidden2_size, output_size=n_actions)
+
+        self.state_memory = np.zeros((self.mem_size, self.input_size), dtype=np.float32)
+        self.new_state_memory = np.zeros((self.mem_size, self.input_size), dtype=np.float32)
+        self.action_memory = np.zeros(self.mem_size, dtype=np.int32)
+        self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
+        self.terminal_memory = np.zeros(self.mem_size, dtype=np.bool)
+
+    def store_transition(self, state, action, reward, state_, done):
+        index = self.mem_cntr % self.mem_size
+        self.state_memory[index] = state
+        self.new_state_memory[index] = state_
+        self.reward_memory[index] = reward
+        self.action_memory[index] = action
+        self.terminal_memory[index] = done
+        self.mem_cntr += 1
+
+    def choose_action(self, observation):
+        if np.random.random() > self.epsilon:
+            state = torch.tensor([observation], dtype=torch.float32).to(self.q_eval.device)
+            actions = self.q_eval.forward(state)
+            action = torch.argmax(actions).item()
+        else:
+            action = np.random.choice(self.action_space)
+        return action
+    
+    def learn(self):
+        self.q_eval.optim.zero_grad()
+        if self.mem_cntr < self.batch_size:
+            return
+        max_mem = min(self.mem_cntr, self.mem_size)
+        batch = np.random.choice(max_mem, self.batch_size, replace=False)
+        batch_index = np.arange(self.batch_size, dtype=np.int32)
+
+        state_batch = self.state_memory[batch]
+        action_batch = self.action_memory[batch]
+        reward_batch = self.reward_memory[batch]
+        new_state_batch = self.new_state_memory[batch]
+        done_batch = self.terminal_memory[batch]
+
+        state_batch = torch.tensor(state_batch).to(self.q_eval.device)
+        action_batch = torch.tensor(action_batch).to(self.q_eval.device)
+        reward_batch = torch.tensor(reward_batch).to(self.q_eval.device)
+        new_state_batch = torch.tensor(new_state_batch).to(self.q_eval.device)
+        done_batch = torch.tensor(done_batch).to(self.q_eval.device)
+
+        # q_eval = self.q_eval.forward(state_batch).gather(1, action_batch.unsqueeze(1)).squeeze(1)
+        q_eval = self.q_eval.forward(state_batch)[batch_index, action_batch]
+        q_next = self.q_eval.forward(new_state_batch).max(dim=1)[0]
+        q_next[done_batch] = 0.0
+        q_target = reward_batch + self.gamma * q_next
+
+        loss = self.q_eval.loss(q_target, q_eval).to(self.q_eval.device)
+        loss.backward()
+        self.q_eval.optim.step()
+
+        self.epsilon = self.epsilon - self.eps_dec if self.epsilon > self.eps_min else self.eps_min
 
 # MAIN FUNCTION
 def main():
@@ -212,11 +242,12 @@ def main():
     print(f"Number of junctions: {num_junctions}")
     lanes = traci.lane.getIDList()
     num_lanes = len(lanes)
-    print(f"Lanes: {lanes}")
+    # print(num_lanes)
+    # print(f"Lanes: {lanes}")
     # edges = traci.edge.getIDList()
     edges = [edge for edge in traci.edge.getIDList() if not edge.startswith(':')]
     num_edges = len(edges)
-    print(f"Edges: {edges}")
+    # print(f"Edges: {edges}")
     end_time = traci.simulation.getEndTime()
     # print(f"End time: {end_time}")
     
@@ -226,9 +257,10 @@ def main():
     hidden2_size = 8
     output_size = 2 * num_junctions
     epochs = 1
+    lr = 0.001
     
     # CREATE MODEL
-    model = TrafficController(input_size, hidden1_size, hidden2_size, output_size)
+    model = TrafficController(input_size, lr, hidden1_size, hidden2_size, output_size)
 
     # TRAIN MODEL
 
@@ -236,12 +268,20 @@ def main():
         step = 0
         total_loss = 0
         num_iters = 0
+        light = traci.trafficlight.getIDList()
+        for l in light:
+            light_test = traci.trafficlight.getAllProgramLogics(l)
+            light_phase_test = traci.trafficlight.getRedYellowGreenState(l)
+            light_time_test = traci.trafficlight.getPhaseDuration(l)
+            # print(f"Light test: {light_test} for light {l}")
+            print(f"Light phase test: {light_phase_test} for light {l}")
+            print(f"Light time test: {light_time_test} for light {l}")
         while step < (end_time + 5):
             # SIMULATION STEP
             traci.simulationStep()
             step += 1
             # num_vehicles(edges)
-            print(f"Step {step}")
+            # print(f"Step {step}")
             # get_vehicle_number(lanes)
             # GET QUEUE LENGTHS AND TIMES
             queue_data, total_length, total_time = queue_info(lanes)
@@ -279,7 +319,7 @@ def main():
             total_loss += loss.item()
             num_iters += 1
 
-            # current_state = traci.trafficlight.getRedYellowGreenState(junctions[0])
+            current_state = traci.trafficlight.getRedYellowGreenState(junctions[0])
             # print(f"Current state: {current_state}")
         
         avg_loss = total_loss / num_iters
