8d85a658060b3f141b1416bd8abed1c100ad858e
BoofinBool <sioldead@mcmaster.ca>
2024-06-03 21:37:15 -0400

    added actions

--- action_list.py
+++ action_list.py
@@ -1,4 +1,49 @@
-actions_4_way = [
+actions_4_way_11_idx = [
+    [60, "GGgrrrGGgrrr", 5, "yyyrrryyyrrr"],
+    [60, "rrrGGgrrrGGg", 5, "rrryyyrrryyy"],
+    [50, "GGgrrrGGgrrr", 5, "yyyrrryyyrrr"],
+    [50, "rrrGGgrrrGGg", 5, "rrryyyrrryyy"],
+    [40, "GGgrrrGGgrrr", 5, "yyyrrryyyrrr"],
+    [40, "rrrGGgrrrGGg", 5, "rrryyyrrryyy"],
+    [30, "GGgrrrGGgrrr", 5, "yyyrrryyyrrr"],
+    [30, "rrrGGgrrrGGg", 5, "rrryyyrrryyy"],
+    [20, "GGgrrrGGgrrr", 5, "yyyrrryyyrrr"],
+    [20, "rrrGGgrrrGGg", 5, "rrryyyrrryyy"],
+    [10, "GGgrrrGGgrrr", 5, "yyyrrryyyrrr"],
+    [10, "rrrGGgrrrGGg", 5, "rrryyyrrryyy"]
+]
+
+actions_4_way_13_idx = [
+    [60, "GGgrrrrGGgrrrr", 5, "yyyrrrryyyrrrr"],
+    [60, "rrrGGGgrrrGGGg", 5, "rrryyyyrrryyyy"],
+    [50, "GGgrrrrGGgrrrr", 5, "yyyrrrryyyrrrr"],
+    [50, "rrrGGGgrrrGGGg", 5, "rrryyyyrrryyyy"],
+    [40, "GGgrrrrGGgrrrr", 5, "yyyrrrryyyrrrr"],
+    [40, "rrrGGGgrrrGGGg", 5, "rrryyyyrrryyyy"],
+    [30, "GGgrrrrGGgrrrr", 5, "yyyrrrryyyrrrr"],
+    [30, "rrrGGGgrrrGGGg", 5, "rrryyyyrrryyyy"],
+    [20, "GGgrrrrGGgrrrr", 5, "yyyrrrryyyrrrr"],
+    [20, "rrrGGGgrrrGGGg", 5, "rrryyyyrrryyyy"],
+    [10, "GGgrrrrGGgrrrr", 5, "yyyrrrryyyrrrr"],
+    [10, "rrrGGGgrrrGGGg", 5, "rrryyyyrrryyyy"]
+]
+
+actions_4_way_14_idx = [
+    [60, "GGGgrrrGGGgrrrr", 5, "yyyyrrryyyyrrrr"],
+    [60, "rrrrGGgrrrrGGGg", 5, "rrrryyyrrrryyyy"],
+    [50, "GGGgrrrGGGgrrrr", 5, "yyyyrrryyyyrrrr"],
+    [50, "rrrrGGgrrrrGGGg", 5, "rrrryyyrrrryyyy"],
+    [40, "GGGgrrrGGGgrrrr", 5, "yyyyrrryyyyrrrr"],
+    [40, "rrrrGGgrrrrGGGg", 5, "rrrryyyrrrryyyy"],
+    [30, "GGGgrrrGGGgrrrr", 5, "yyyyrrryyyyrrrr"],
+    [30, "rrrrGGgrrrrGGGg", 5, "rrrryyyrrrryyyy"],
+    [20, "GGGgrrrGGGgrrrr", 5, "yyyyrrryyyyrrrr"],
+    [20, "rrrrGGgrrrrGGGg", 5, "rrrryyyrrrryyyy"],
+    [10, "GGGgrrrGGGgrrrr", 5, "yyyyrrryyyyrrrr"],
+    [10, "rrrrGGgrrrrGGGg", 5, "rrrryyyrrrryyyy"]
+]
+
+actions_4_way_15_idx = [
     [60, "rrrrGGGgrrrrGGGg", 5, "rrrryyyyrrrryyyy"],
     [60, "GGGgrrrrGGGgrrrr", 5, "yyyyrrrryyyyrrrr"],
     [50, "rrrrGGGgrrrrGGGg", 5, "rrrryyyyrrrryyyy"],
@@ -12,6 +57,7 @@ actions_4_way = [
     [10, "rrrrGGGgrrrrGGGg", 5, "rrrryyyyrrrryyyy"],
     [10, "GGGgrrrrGGGgrrrr", 5, "yyyyrrrryyyyrrrr"]
 ]
+
 
 actions_3_way_7_idx = [
     [60, "GGgrrGGG", 5, "yyyrryyy"],
